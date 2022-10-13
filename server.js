699f81777e6df9c0f0ce5c55bd8c0a273944ef66
Camilla Rees <camilla.c.rees@gmail.com>
2022-10-12 20:24:58 -0600

    refactored weather and movie API pathways

--- server.js
+++ server.js
@@ -4,8 +4,10 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const weatherData = require('/weather');
-const movieData = require('/movies')
+const axios = require('axios');
+
+// const weatherData = require('/weather');
+// const movieData = require('/movies')
 
 // create an instance of an Express server
 const app = express();
@@ -23,81 +25,76 @@ app.get('/', (request, response) => {
 });
 
 // define an endpoint that gets the weather data and returns it to React
-app.get('/weather', (request, response, next) => {
+app.get('/weather', async (request, response, next) => {
+    const lat = request.query.lat;
+    const lon = request.query.lon;
+    // console.log(lat, lon);
     try {
-//grab the searchQuery from the request object
-// notice that query parameter is named type
-// type is the name of query parameter we must send along with Axios from React in order to ask for data from our server 
-// baseURL, endpoint, query, queryParameters
-const weatherURL = `https://api.weatherbit.io/v2.0/current?key=21634fd4e5154fb5a84ae23da4570a59&lat=38.123&lon=-78.543`
-const weatherResponse = await axios.get(weatherUrl);
-const weatherArray = weatherResponse.data.map(forecast => new Forecast(forecast));
-const cityName = weather.getWeather();
+        //grab the searchQuery from the request object 
+        // baseURL, endpoint, query, queryParameters
+        const weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=5&key=21634fd4e5154fb5a84ae23da4570a59`
+        const weatherResponse = await axios.get(weatherURL);
+        console.log(weatherResponse.data.data[0]);
+        const weatherArray = weatherResponse.data.data.map(forecast => new Forecast(forecast));
 
-response.status(200).send(cityName);
-} catch(error) {
-    next(error.message);
-}
+        response.status(200).send(weatherArray);
+    } catch (error) {
+        next(error.message);
+    }
 });
 
-app.get('/movies', (request, response, next) => {
+class Forecast {
+    constructor(forecast) {
+        // find method to find the type of list we want to return
+        this.date = forecast.datetime;
+        this.description = forecast.weather.description;
+    };
+
+}
+
+app.get('/movies', async (request, response, next) => {
+    const movie = request.query.city;
+    console.log(movie);
     try {
         const moviesURL = `https://api.themoviedb.org/3/movie/76341?api_key=c0954586125a696f1e442215a8fb87dc&query=seattle`
         const movieResponse = await axios.get(moviesURL);
-        const movieArray = movieResponse.data.map(movie => new Movie (movie));
+        const movieArray = movieResponse.data.data.map(movie => new Movie(movie));
+        console.log(movieResponse.data.data[0]);
+        response.status(200).send(movieArray);
     } catch (error) {
         next(error.message);
-    }}
+    }
+}
 );
 
-
-class Forecast {
-    constructor (forecast) {
-        // find method to find the type of list we want to return
-        let { lat, lon, city_name, data } = weatherResponse.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
-        this.cityName = forecast.city_name; 
-        this.lat = forecast.data.lat;
-        this.lon = forecast.data.lon;
-    };
-
-
-// method that gets just the name and desc properties from our item objects
-getWeather() {
-    return this.forecast.map(day => ({
-        forecast: day.weather.description,
-        date: day.datetime
-    }));
-}
-}
-
-class Movies {
-    constructor (movie) {
-        this.movie = [{}]
+class Movie {
+    constructor(movie) {
+        this.title = movie.title;
+        this.overview = movie.overview;
+        this.average_votes = movie.average_votes;
+        this.total_votes = movie.total_votes;
+        this.image_url = movie.image_url;
+        this.popularity = movie.popularity;
+        this.released_on = movie.released_on;
     }
-};
-
-getMovies() {
-    return this.movies.data.map(movie => ({
-        movies: this.movies.data
-    }))
 }
 
 app.get('/fakeError', (request, response, next) => {
     try {
         const nonexistentData = require('./nonexistentData/js');
         response.send(nonexistentData);
-    } catch(error) {
+    } catch (error) {
         next(error.message);
-    
-        }
-    }
 
-    // error handling middleware must be the last app.use() defined in the server file
-    app.use((error, request, response, next) => {
-        console.log(error);
-        response.status(500).send(error);
-    });
+    }
+}
 )
+
+// error handling middleware must be the last app.use() defined in the server file
+app.use((error, request, response, next) => {
+    console.log(error);
+    response.status(500).send(error);
+});
 
 
 // this line of code needs to be the last line in the file
